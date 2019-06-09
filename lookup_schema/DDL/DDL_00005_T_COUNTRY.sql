218f12b0aa208cfe2912e150d267ba65e324e0d2
kisho <kisho@DESKTOP-G0O6EAI>
2019-06-09 20:15:10 +0200

    updated country, currency SQLS. Deleted APP_COUNTRY SQLS

---
+++ lookup_schema/DDL/DDL_00005_T_COUNTRY.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS T_COUNTRY;
+
+CREATE TABLE IF NOT EXISTS T_COUNTRY (
+  ID smallint NOT NULL AUTO_INCREMENT COMMENT 'Two-letter country code (ISO 3166-1 alpha-2)',
+  COUNTRY_NAME varchar(20) UNIQUE NOT NULL COMMENT 'English country name',
+  COUNTRY_CODE_ISO2 char(2) UNIQUE NOT NULL COMMENT 'Three-letter country code (ISO 3166-1 alpha-2)',
+  COUNTRY_CODE_ISO3 char(3) NOT NULL,
+  COUNTRY_NUM SMALLINT(3) ZEROFILL NOT NULL COMMENT 'Three-digit country number (ISO 3166-1 numeric)',
+  CONTINENT_CODE VARCHAR(2),
+  DIAL_CODE varchar(5) NOT NULL,
+  IS_ACTIVE boolean DEFAULT 0 ,
+PRIMARY KEY (`ID`),
+  KEY `continent_code` (`continent_code`),
+  CONSTRAINT `fk_continents` FOREIGN KEY (`continent_code`) REFERENCES `T_CONTINENTS` (`code`)
+) ENGINE=InnoDB;
---
+++ lookup_schema/DML/DML_00004_T_CONTINENTS.sql
@@ -0,0 +1,8 @@
+INSERT INTO T_CONTINENTS VALUES
+  ('AF', 'Africa'),
+  ('AS', 'Asia'),
+  ('EU', 'Europe'),
+  ('NA', 'North America'),
+  ('SA', 'South America'),
+  ('OC', 'Oceania'),
+  ('AN', 'Antarctica');
